cdb56f68abee951877518e443d9ade9cab73a585
이진유 <22yuu@naver.com>
2022-02-16 22:13:17 +0900

    4.6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  /*
+    React.StrictMode 란?
+    자바스크립트의 use strict와 동일한 개념으로 코딩이 잘못된 부분이 있으면 에러 메시지를 출력해주는 역할을 한다.
+    생략해도 무방하지만 개발단계에서는 꼭 사용하는 것이 좋으며 어차피 해당 부분을 생략하지 않고 배포를 하여도
+    배포된 상태에서는 StrictMode가 활성화되지 않아서 사용자가 콘솔에서 에러 메세지를 확인할 수 없다.
+  */
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
