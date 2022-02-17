6bf00d5353888b90c03cbbfef87f2712c9fc42d0
이진유 <22yuu@naver.com>
2022-02-17 23:46:24 +0900

    4.9

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
   handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야함.
-    this.setState({ count: this.state.count + 1 });
-    /*
-        this.state.count++, this.state.count += 1 처럼 카운트를 증가시켜주면 되지 않나 라는 의문을 가질 수 있는데,
-        위의 방법처럼 오브젝트 안에 있는 데이터를 업데이트하게 되면 리액트에서 해당 데이터의 값이 업데이트 되었는지 안되었는지
-        알 수 없다.
-        그래서 꼭 리액트에서 데이터를 업데이트할 때는 setState()라는 함수를 사용해야 한다.
-    */
+    this.props.onIncrement(this.props.habit);
   };
+
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
   };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
   render() {
     const { name, count } = this.props.habit;
     return (
@@ -36,7 +31,10 @@ class Habit extends Component {
         >
           <i class="fa-solid fa-square-minus"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i class="fas fa-trash"></i>
         </button>
       </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -9,11 +9,35 @@ class Habits extends Component {
       { id: 3, name: "Coding", count: 0 },
     ],
   };
+
+  handleIncrement = (habit) => {
+    console.log(`handleIncrement ${habit.name}`);
+    habit.count += 1;
+    console.log(`count ${habit.count}`);
+  };
+
+  handleDecrement = (habit) => {
+    console.log(`handleDecrement ${habit.name}`);
+    habit.count -= 1;
+  };
+
+  handleDelete = (habit) => {
+    const idx = this.state.habits.findIndex((e) => e.id === habit.id);
+    let array = [...this.state.habits];
+    array.splice(idx, 1);
+  };
+
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => (
-          <Habit key={habit.id} habit={habit} />
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
         ))}
       </ul>
     );
